1a7e399f1c36051522be040a5b6f8a5a48ceeaf6
sunnyteotia15 <140685546+sunnyteotia15@users.noreply.github.com>
2023-07-27 18:41:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,10 +15,14 @@ provider "azurerm" {
 terraform {
   backend "azurerm" {}
 }
+
+#Create resource group for all other components
 resource "azurerm_resource_group" "resourceGroup" {
   name     = var.resourceGroupName
   location = var.location
 }
+
+#create virtual network & subnets for database and function app
 resource "azurerm_virtual_network" "vnet" {
   name                = var.vnetName
   resource_group_name = azurerm_resource_group.resourceGroup.name
@@ -46,6 +50,8 @@ resource "azurerm_subnet" "db-subnet" {
   resource_group_name  = azurerm_resource_group.resourceGroup.name
   address_prefixes     = var.dbSubnetAddressSpace
 }
+
+#create sql server to host sql database
 resource "azurerm_sql_server" "sqlServer" {
   name = var.sqlServerName
   resource_group_name = azurerm_resource_group.resourceGroup.name
@@ -54,9 +60,41 @@ resource "azurerm_sql_server" "sqlServer" {
   administrator_login_password = var.sqlServer_password
 }
 
+#create sql databse
 resource "azurerm_sql_database" "db" {
   name                = var.databaseName
   resource_group_name = azurerm_resource_group.resourceGroup.name
   location            = azurerm_resource_group.resourceGroup.location
   server_name         = azurerm_sql_server.sqlServer.name
+}
+
+#create app service plan to host azure function app
+resource "azurerm_app_service_plan" "appServicePlan" {
+  name                = var.appServicePlan
+  location            = azurerm_resource_group.resourceGroup.location
+  resource_group_name = azurerm_resource_group.resourceGroup.name
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+
+#create storage account as a pre-requisite for function app
+resource "azurerm_storage_account" "storageAccount" {
+  name                     = "var.storageAccountName"
+  resource_group_name      = azurerm_resource_group.resourceGroup.name
+  location                 = azurerm_resource_group.resourceGroup.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+#create azure function app for the Front end
+resource "azurerm_function_app" "app" {
+  name                       = var.appName
+  location                   = azurerm_resource_group.resource_group.location
+  resource_group_name        = azurerm_resource_group.resource_group.name
+  app_service_plan_id        = azurerm_app_service_plan.appServicePlan.id
+  storage_account_name       = azurerm_storage_account.storageAccount.name
+  storage_account_access_key = azurerm_storage_account.storageAccount.primary_access_key
 }
